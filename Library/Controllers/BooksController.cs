19b6ac904c0a608ec60579989226fdb6668cdeda
gvsn1999 <gvsn1999@users.noreply.github.com>
2023-02-02 19:19:31 +0100

    Initial commit

---
+++ Library/Controllers/BooksController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Library.Context;
+using Library.Models;
+using Library.ModelView;
+
+namespace Library.Controllers
+{
+    public class BooksController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index(string search = null)
+        {
+            if (search != null)
+            {
+                var model = db.Books.Where(b => b.Title.Contains(search)).ToList();
+                return View(model);
+            }
+            return View(db.Books.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        // GET: Books/Create
+        public ActionResult Create()
+        {
+            var model = new BookCreateModelView();
+            model.Categories = db.Categories.Select(c => new SelectListItem { Text = c.Name, Value = c.CategoryId.ToString() });
+            model.Date = DateTime.Now;
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BookId,Title,Author,Description,CategoryId,Date,ISBN,Available")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Books.Add(book);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(book);
+        }
+
+        
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            var model = new BookCreateModelView();
+            model.BookId = book.BookId;
+            model.Title = book.Title;
+            model.Author = book.Author;
+            model.Description = book.Description;
+            model.CategoryId = book.CategoryId;
+            model.Date = book.Date;
+            model.ISBN = book.ISBN;
+            model.Available = book.Available;
+            model.Categories = db.Categories.Select(c => new SelectListItem { Text = c.Name, Value = c.CategoryId.ToString() });
+
+            return View(model);
+            //return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BookId,Title,Author,Description,CategoryId,Date,ISBN,Available")] Book book)
+        {
+            if (ModelState.IsValid)
+                {
+                    db.Entry(book).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            return View(book);
+        }
+
+        
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book = db.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Book book = db.Books.Find(id);
+            db.Books.Remove(book);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Library/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
